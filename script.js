d1f57c4c0f3c5dc4f999272ed69789708a08d957
yugmani <yugmani@yahoo.com>
2022-07-03 12:07:43 -0700

    javaScript

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('Hello!');
+const boxes = document.querySelectorAll('.box');
+window.addEventListener('scroll', boxesCheck);
+
+boxesCheck();
+
+function boxesCheck() {
+  const bottomTrigger = (window.innerHeight / 5) * 4;
+  boxes.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top;
+
+    if (boxTop < bottomTrigger) {
+      box.classList.add('show');
+    } else {
+      box.classList.remove('show');
+    }
+  });
+}
